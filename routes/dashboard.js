6fd4d40f1c53895f3439a4d0f8e89aea30ee33f2
Omar Terrazas <terrazas.omar@gmail.com>
2025-06-19 13:20:08 -0600

    Add logout functionality: implement logout route and update menu link

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -429,5 +429,15 @@ module.exports = (app) => {
     }
   });
 
+  app.get('/cf/logout', (req, res) => {
+    req.session.destroy(err => {
+      if (err) {
+        console.error('Error al cerrar sesión:', err);
+        return res.status(500).send('Error al cerrar sesión');
+      }
+      res.redirect('/cf/');
+    });
+  });
+
   
 };
